7f9490663f0c3abc3ac8c2f676a89b74f3fccea5
Mocha-- <xibowangmelb@gmail.com>
2015-11-02 11:49:12 +1100

    final agian

--- Client/bin/login2.sh
+++
@@ -1,1 +0,0 @@
-java -cp ../lib/org.json.jar:../lib/args4j-2.32.jar:. Client localhost -p 4444 -id user2 -password 123
--- Server/bin/hehehe.sh
+++
@@ -1,2 +0,0 @@
-#java -cp ./lib/org.json.jar:./lib/args4j-2.32.jar:./lib/java-jwt-2.0.1.jar:./bin ./bin/Server -p 4444
-java -cp ../lib/org.json.jar:../lib/args4j-2.32.jar:../lib/java-jwt-2.0.1.jar:. Server -p 4444
